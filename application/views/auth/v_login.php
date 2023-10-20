b6144d71be6a8485198ef6db89fb76ee11dde98c
Yuli Resmawati <yhuli.resmawati@gmail.com>
2023-10-20 11:56:10 +0700

    bis

--- application/views/auth/v_login.php
+++ application/views/auth/v_login.php
@@ -1,58 +1,43 @@
-<div class="section page-banner-section bg-color-1">
-    <img class="shape-1" src="<?= $theme_path ?>/images/shape/shape-5.png" alt="shape">
-    <img class="shape-2" src="<?= $theme_path ?>/images/shape/shape-6.png" alt="shape">
-    <img class="shape-3" src="<?= $theme_path ?>/images/shape/shape-7.png" alt="shape">
-    <img class="shape-4" src="<?= $theme_path ?>/images/shape/shape-21.png" alt="shape">
-    <img class="shape-5" src="<?= $theme_path ?>/images/shape/shape-21.png" alt="shape">
-
-    <div class="container">
-        <div class="page-banner-content">
-            <h2 class="title">Halaman Login</h2>
-            <ul class="breadcrumb justify-content-center">
-                <li class="breadcrumb-item"><a href="<?= base_url('home') ?>">Home</a></li>
-                <li class="breadcrumb-item active">Login</li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="section section-padding">
-    <div class="container">
-        <div class="login-register-wrapper">
-            <div class="row gx-5">
-                <div class="col-lg-6 mx-auto">
-                    <div class="login-register-box">
-                        <div class="section-title">
-                            <h2 class="title text-center">Masukkan Kredensial Anda</h2>
-                            <hr>
-                        </div>
-                        <div class="login-register-form">
-                            <?= form_open('auth/do_login', 'id="formLogin"'); ?>
-                                <input type="hidden" class="g-recaptcha-response" name="g-recaptcha-response">
-                                <div class="single-form">
-                                    <input type="text" class="form-control" name="silatpendidikan_username" id="silatpendidikan_username" placeholder="nama pengguna ">
-                                </div>
-                                <div class="single-form">
-                                    <input type="password" class="form-control" name="silatpendidikan_password" id="silatpendidikan_password" placeholder="kata sandi ">
-                                </div>
-                                <div class="single-form form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                    <label for="flexCheckDefault">Lihat Kata Sandi</label>
-                                </div>
-                                <div class="single-form">
-                                    <button class="btn btn-primary btn-hover-heading-color w-100" id="submit-login">
-                                        <span class="spinner-border spinner-border-sm mr-1" id="spinner-status" role="status" aria-hidden="true" style="display:none"></span>
-                                        <i class="bi bi-unlock me-2" id="login-icon"></i> <span id="text-button">Login</span> 
-                                    </button>
-                                </div>
-                            <?= form_close(); ?>
-                        </div>
-                    </div>
-                </div>
+<div class="main-banner" style="background: white;padding: 130px 0px 50px 0px;">
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="right-image">
+                <img src="<?= $theme_path ?>/assets/images/mobile-login.jpg" alt="">
             </div>
         </div>
+        <div class="col-lg-6">
+          <div class="card shadow-2-strong" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+              <h3 class="mb-3">Selamat Datang!</h3>
+                <p>Butuh bantuan informasi login? <a class="ms-2" href="#">Hubungi Kami</a></p>
+              <hr class="my-4">
+              <?= form_open('auth/do_login', 'id="formLogin"'); ?>
+                  <input type="hidden" class="g-recaptcha-response" name="g-recaptcha-response">
+                  <div class="form-outline mb-4" >
+                    <input type="text" name="silatpendidikan_username" id="silatpendidikan_username" class="form-control form-control-lg" placeholder="Nama Pengguna" style="font-size: small;"/>
+                  </div>
+                  <div class="form-outline mb-4">
+                    <input type="password" name="silatpendidikan_password" id="silatpendidikan_password" class="form-control form-control-lg"  placeholder="Kata Sandi" style="font-size: small;"/>
+                  </div>
+                  <!-- Checkbox -->
+                  <div class="form-check d-flex justify-content-start mb-4">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault" />
+                    <label class="form-check-label" for="flexCheckDefault" style="font-size: small;"> Lihat Kata Sandi </label>
+                  </div>
+                  <div class="d-grid gap-2">
+                    <button class="btn btn-lg btn-block text-white w-100" id="submit-login" style="background:#ff4f5a">
+                      <span class="spinner-border spinner-border-sm mr-1" id="spinner-status" role="status" aria-hidden="true" style="display:none"></span>
+                      <i<i class="fa fa-sign-in" id="login-icon"></i> <span id="text-button">Login</span> 
+                    </button>
+                  </div>
+              <?= form_close(); ?>
+            </div>
+          </div>
+        </div>
     </div>
+  </div>
 </div>
-
 <script type="text/javascript">
     $('#flexCheckDefault').click(function() {
         if ('password' == $('#silatpendidikan_password').attr('type')) {
